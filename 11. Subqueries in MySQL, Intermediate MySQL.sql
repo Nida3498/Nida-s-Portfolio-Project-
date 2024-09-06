8dbb5a9a714a6358df5ef324f220754ae2413abe
Nida <nida.ansari@csuglobal.edu>
2024-09-06 11:46:20 -0400

    Create 11. Subqueries in MySQL, Intermediate MySQL.sql

---
+++ 11. Subqueries in MySQL, Intermediate MySQL.sql
@@ -0,0 +1,71 @@
+-- Subqueries
+-- A query within another query
+
+SELECT * 
+FROM employee_demographics;
+
+SELECT *
+FROM employee_salary;
+
+SELECT * 
+FROM employee_demographics
+WHERE employee_id IN
+					(SELECT employee_id
+						FROM employee_salary
+						WHERE dept_id = 1)
+;
+
+SELECT *
+FROM employee_salary;
+
+
+SELECT first_name, salary,
+(SELECT AVG(salary)
+FROM employee_salary)
+FROM employee_salary;
+
+SELECT *
+FROM employee_demographics;
+
+SELECT gender, AVG(age), MAX(age), MIN(age), COUNT(age)
+FROM employee_demographics
+GROUP BY gender;
+
+
+SELECT *
+FROM
+(SELECT gender, AVG(age), MAX(age), MIN(age), COUNT(age)
+FROM employee_demographics
+GROUP BY gender) AS Agg_table
+;
+
+
+SELECT gender, AVG(`MAX(age)`)
+FROM
+(SELECT gender, 
+AVG(age) AS avg_age,
+MAX(age),
+MIN(age) AS min_age, 
+COUNT(age) AS count_age
+FROM employee_demographics
+GROUP BY gender) AS Agg_table
+GROUP BY gender
+;
+
+SELECT AVG(`MAX(age)`)
+FROM
+(SELECT gender, AVG(age), MAX(age), MIN(age), COUNT(age)
+FROM employee_demographics
+GROUP BY gender) AS Agg_table
+;
+
+SELECT AVG(max_age)
+FROM
+(SELECT gender, 
+AVG(age) AS avg_age,
+MAX(age) AS max_age,
+MIN(age) AS min_age, 
+COUNT(age)
+FROM employee_demographics
+GROUP BY gender) AS Agg_table
+;
